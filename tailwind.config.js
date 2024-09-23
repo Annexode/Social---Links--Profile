e634dcb725562a7337421d87f4361210798721da
Annexode <annexode@gmail.com>
2024-09-23 18:20:16 +0200

    Social links

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        baclColor: ["#1f1f1f"],
+        linksBack: ["#333333"],
+        lighttYellow: ["#c4f82a"],
+      },
+      fontFamily: {
+        inter: ["Inter"],
+      },
+    },
+  },
+  plugins: [],
+};
